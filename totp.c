07d83b3dfb800a5e01999bff39eb07079bafe2fe
misc147 <misc7@posteo.net>
2025-02-25 12:28:52 +0000

    fix fmt handling

--- totp.c
+++ totp.c
@@ -244,7 +244,7 @@ void dzen(int token, int nexttoken, int seconds, char **pexec){
 				//eprintsl("Error, couldn't execute: ",*pexec);
 				fprintf(stderr,"Error, couldn't execute: ");
 				while (*pexec){
-					write( stderr, *pexec, strlen( *pexec ) );
+					write( STDERR_FILENO, *pexec, strlen( *pexec ) );
 					*pexec++;
 				}
 				fprintf(stderr,"\n");
